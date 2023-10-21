4844e5925396ee5fbae13d4f9babaa02dd1f6ae1
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-10-21 19:28:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+
+const information= document.getElementById("browser-info");
+
+information.innerText="You are using "+window.navigator.appName+" version "+window.navigator.appVersion
